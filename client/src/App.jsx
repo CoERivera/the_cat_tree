03b230064262aaf1ec5c49f22805965c4d047460
Christopher Rivera <riveramercado.christopher@gmail.com>
2023-01-30 20:18:26 -0400

    Added logos and font

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.css';
+
+function App() {
+  return (
+    <div className="container mt-5">
+      <h1>The Cat Tree</h1>
+      <img src="https://i.imgur.com/uBAk6EB.png" alt="cat_tree" />
+    </div>
+  )
+}
+
+export default App
